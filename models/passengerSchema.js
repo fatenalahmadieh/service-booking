4ec2133cba3141a234ab45db9a5ffbfb304d0027
fatenalahmadieh <fatenahmadieh@gmail.com>
2026-06-15 20:18:45 +0300

    Create passengerSchema for MongoDB model

---
+++ models/passengerSchema.js
@@ -0,0 +1,22 @@
+const { default: mongoose } = require("mongoose");
+
+const passengerSchema = new mongoose.Schema({
+    fullName : {
+        type: String,
+        required: true
+    },
+    passportNumber: {
+        type: String,
+        required:true
+    },
+    nationality: {
+        type: String,
+        required: true
+    },
+    dateOfBirth:{
+        type: Date
+    }
+}
+{ timestamps: true }
+);
+module.exports = mongoose.model("Passenger", passengerSchema);
