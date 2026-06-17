d21343488e22317f9b623d756f819252299114e2
Ali Chible <alishibli04@gmail.com>
2026-06-17 16:40:48 +0300

    Booking Logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,30 @@
 const User = require("../models/userSchema");
+const Flight=require("../models/flightSchema");
+const Booking= require("../models/bookingSchema");
 exports.booking=async(req,res)=>{
     try{
+        const {userID, flightID,seatNumber, totalPrice}=req.body;
+        const flight=await Flight.findById(flightID);
+        if(!flight){
+            return res.status(404).json({message:"Flight not found"});
+        }
+        if(flight.capacity<=0){
+            return res.status(400).json({message:"Flight is full"});
+        }
+        flight.capacity-=1;
+        await flight.save();
         
+        const bookedFlight=new Booking({
+            seatNumber,
+            bookingStatus:'confirmed',
+            totalPrice,
+            passenger:userID,
+            flight:flightID,
+        });
+        await bookedFlight.save();
+        res.status(201).json({
+            message:"New Booking Added Successfully"
+        });
     }catch(err){
         console.log(err);
         res.status(500).json({message:err.message});
--- models/passengerSchema.js
+++ models/passengerSchema.js
@@ -1,4 +1,7 @@
-const passengerSchema = new mongoose.Schema({
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+const passengerSchema = new Schema({
     fullName : {
         type: String,
         required: true
@@ -14,7 +17,7 @@ const passengerSchema = new mongoose.Schema({
     dateOfBirth:{
         type: Date
     }
-}
+},
 { timestamps: true }
 );
 module.exports = mongoose.model("Passenger", passengerSchema);
