551cba3b2ec90364b60deb628a08b01b9ee39c58
fatenalahmadieh <fatenahmadieh@gmail.com>
2026-06-23 17:44:36 +0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,87 @@ const Flight = require("../models/flightSchema");
 const Booking = require("../models/bookingSchema");
 const userSchema = require("../models/userSchema");
 
+// create /api/User/:id
+// craete user
+exports.createUser = async (req, res) => {
+    try {
+        const userCheck = await User.findOne({$or: [
+            {firstName: req.body["firstName"]},
+            {lastName: req.body["lastName"]},
+            {email: req.body["email"]},
+            {password: req.body["password"]},
+            {nationality: req.body["nationality"]},
+            {dateOfBirth: req.body["dateOfBirth"]},
+            {userType: req.body["userType"]},
+    ]});
+    if(user){
+            return res
+            .status(409)
+            .json({message: "User is already found"});
+    };
+    const newUser = await userSchema.create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: req.body.password,
+            nationality: req.body.nationality,
+            dateOfBirth: req.body.dateOfBirth,
+            userType: req.body.userType,
+            pilot: req.body.userType === 'pilot' ? req.body.pilot : undefined,
+            passenger: req.body.userType === 'passenger' ? req.body.pilot : undefined,
+            host: req.body.userType === 'host' ? req.body.pilot : undefined,
+            admin: req.body.userType === 'admin' ? req.body.pilot : undefined,
+    });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: err.message });
+    }
+};
 
+// Update /api/User/:id
+// update user
+exports.updateUser = async (req, res) => {
+    try {
+        const userCheck = await User.findOne({$or: [
+            {firstName: req.body["firstName"]},
+            {lastName: req.body["lastName"]},
+            {email: req.body["email"]},
+            {password: req.body["password"]},
+            {nationality: req.body["nationality"]},
+            {dateOfBirth: req.body["dateOfBirth"]},
+            {userType: req.body["userType"]},
+        ]});
+        if(user){
+            return res
+            .status(409)
+            .json({message: "User is already found"});
+        };
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params["id"],
+            {
+            ffirstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: req.body.password,
+            nationality: req.body.nationality,
+            dateOfBirth: req.body.dateOfBirth,
+            userType: req.body.userType,
+            pilot: req.body.userType === 'pilot' ? req.body.pilot : undefined,
+            passenger: req.body.userType === 'passenger' ? req.body.pilot : undefined,
+            host: req.body.userType === 'host' ? req.body.pilot : undefined,
+            admin: req.body.userType === 'admin' ? req.body.pilot : undefined,
+            },
+            { new: true }
+            );
+        return res.status(200).json({
+            message: "User updated successfully",
+            data: updatedUser
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: err.message });
+    }
+};
 
 
 
